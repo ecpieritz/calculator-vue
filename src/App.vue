fe486883e2002e46ea3e6a38a775c220e290289d
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-08-24 10:40:10 -0300

    Added code base

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-VariableFont_wght.ttf");
+}
+
+*{
+  font-family: 'RobotoMono', monospace;
+}
+
+body{
+  margin: 0;
+}
+
+#app{
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: #fff;
+  background: linear-gradient(90deg, rgba(43,23,110,1) 10%, rgba(144,118,233,1) 90%);
+}
+</style>
