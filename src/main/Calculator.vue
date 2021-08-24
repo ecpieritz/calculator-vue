9c0867b4d23a3d02942977c263d51f235e354c73
Emilyn Pieritz <66131506+ecpieritz@users.noreply.github.com>
2021-08-24 12:11:48 -0300

    Added setOperation function

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -22,51 +22,72 @@
 </template>
 
 <script>
-import Display from "../components/Display"
-import Button from "../components/Button"
+import Display from "../components/Display";
+import Button from "../components/Button";
 
 export default {
-  data: function(){
-    return{
+  data: function () {
+    return {
       displayValue: "0",
       clearDisplay: false,
       operation: null,
-      values:[0, 0],
-      current: 0
-    }
+      values: [0, 0],
+      current: 0,
+    };
   },
-  components:{Display, Button},
+  components: { Display, Button },
   methods: {
-    clearMemory(){
-      Object.assign(this.$data, this.$options.data())
+    clearMemory() {
+      Object.assign(this.$data, this.$options.data());
     },
-    setOperation(){
-      
+    setOperation(operation) {
+      if (this.current === 0) {
+        this.operation = operation;
+        this.current = 1;
+        this.clearDisplay = true;
+      } else {
+        const equals = operation === "=";
+        const currentOperation = this.operation;
+
+        try {
+          this.values[0] = eval(
+            `${this.value[0]} ${currentOperation} ${this.values[1]}`
+          );
+        } catch (e) {
+          this.$emit("onError", e);
+        }
+
+        this.values[1] = 0;
+        this.displayValue = this.values[0];
+        this.operation = equals ? null : operation;
+        this.current = equals ? 0 : 1;
+        this.clearDisplay = !equals;
+      }
     },
-    addDigit(n){
-      if(n==='.' && this.displayValue.includes('.')){
-        return
+    addDigit(n) {
+      if (n === "." && this.displayValue.includes(".")) {
+        return;
       }
 
-      const clearDisplay = this.displayValue === "0" || this.clearDisplay
-      const currentValue = clearDisplay ? '' : this.displayValue
-      const displayValue = currentValue + n
+      const clearDisplay = this.displayValue === "0" || this.clearDisplay;
+      const currentValue = clearDisplay ? "" : this.displayValue;
+      const displayValue = currentValue + n;
 
-      this.displayValue = displayValue
-      this.clearDisplay = false
+      this.displayValue = displayValue;
+      this.clearDisplay = false;
 
-      if (n != '.'){
-        const i = this.current
-        const newValue = parseFloat(displayValue)
-        this.values[i] = newValue
+      if (n !== ".") {
+        const i = this.current;
+        const newValue = parseFloat(displayValue);
+        this.values[i] = newValue;
       }
     },
-  }
-}
+  },
+};
 </script>
 
 <style>
-.calculator{
+.calculator {
   height: 320px;
   width: 235px;
   border-radius: 5px;
@@ -74,6 +95,6 @@ export default {
 
   display: grid;
   grid-template-columns: repeat(4, 25%);
-  grid-template-rows: 1fr 48px 48px 48px 48px 48px; 
+  grid-template-rows: 1fr 48px 48px 48px 48px 48px;
 }
 </style>
